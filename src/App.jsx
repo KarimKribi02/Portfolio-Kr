fe8c9c1eb74f6183c452cb0f32613644507c2e2f
KarimKribi02 <abdelkarimkribi@gmail.com>
2024-12-27 15:35:30 +0100

    fin de portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-<<<<<<< HEAD
+
 import Technologies from "./Components/Technologies.jsx";
 import Experience from "./Components/Experience.jsx";
-=======
+
 import About from "./Components/About";
 import Contact from "./Components/Contact";
->>>>>>> 96c57ea19283940135a881d8ddf567fca2ac638a
+import Projects from "./Components/Projects.jsx";
 function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased slec selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-<<<<<<< HEAD
-
-=======
->>>>>>> 96c57ea19283940135a881d8ddf567fca2ac638a
       </div>
       <div className="container mx-auto px-8">
         <Navbar/>
         <Hero/>
-<<<<<<< HEAD
-        <Technologies/>
-        <Experience></Experience>
-=======
         <About/>
+        <Technologies/>
+        <Experience/>
+        <Projects/>
+
+        
         <Contact/>
->>>>>>> 96c57ea19283940135a881d8ddf567fca2ac638a
       </div>
     
     
@@ -36,5 +32,3 @@ function App() {
 }
 
 export default App;
-
-
--- src/constants/index.js
+++ src/constants/index.js
@@ -10,64 +10,65 @@ I am a dedicated and versatile full stack developer with a passion for creating 
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2024 - Present",
+    role: "Product Owner",
+    company: "OFPPT",
+    description: `le porte-parole des parties prenantes et des utilisateurs finaux. Il définit la vision du produit et veille à ce que l’équipe Scrum travaille sur les fonctionnalités les plus prioritaires.`,
+    technologies: ["Tailwind", "React.js", "PHP", "MySQL"],
   },
   {
-    year: "2022 - 2023",
+    year: "2023 - 2024",
     role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    company: "OFPPT",
+    description: `Designed and developed user interfaces for web applications using JavaScript and HTML/CSS.`,
+    technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
   },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+  // {
+  //   year: "2021 - 2022",
+  //   role: "Full Stack Developer",
+  //   company: "Facebook",
+  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  // },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Site Web DriveGo",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Projet DriveGo consiste à développer une plateforme web de location de voitures.",
+
+    technologies: ["PHP", "Tailwind", "React.js", "MySQL",],
   },
   {
-    title: "Task Management App",
+    title: "Site Web Ruby Store",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Conception et réalisation d’une boutique E-commerce.",
+    technologies: ["HTML", "CSS", "JavaScript", " Bootstrap"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["React.js", "Tailwind", "Motion",],
   },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+  // {
+  //   title: "Blogging Platform",
+  //   image: project4,
+  //   description:
+  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  // },
 ];
 
 export const CONTACT = {
